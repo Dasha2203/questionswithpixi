1c21e437a9ce2bfe92137d98970ad1283b7a6783
Dasha2203 <44381104+Dasha2203@users.noreply.github.com>
2024-09-16 20:06:45 +0300

    feat: add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./Screens/Main";
+import Question from "./Screens/Question";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+  },
+  {
+    path: "question/:id",
+    element: <Question />,
+  },
+]);
 
 function App() {
 
   return (
     <>
-      APP
+      <RouterProvider router={router} />
     </>
   )
 }
