3b3c6ace8a6d2ea55b21f8f4be3c4bf070045f7f
Dasha2203 <44381104+Dasha2203@users.noreply.github.com>
2024-09-19 02:40:37 +0300

    init: pixi

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "./Screens/Main";
 import Question from "./Screens/Question";
 import Result from "./Screens/Result";
+import { lazy, Suspense } from "react";
+import { Spinner } from "react-bootstrap";
+
+const GamePage = lazy(() => import('./Screens/Game'));
 
 const router = createBrowserRouter([
   {
@@ -16,13 +20,19 @@ const router = createBrowserRouter([
     path: "result",
     element: <Result />,
   },
+  {
+    path: "game",
+    element: <GamePage />,
+  },
 ]);
 
 function App() {
-  
+
   return (
     <div className="bg-dark">
-      <RouterProvider router={router} />
+      <Suspense fallback={<Spinner animation="border" variant="light" />}>
+        <RouterProvider router={router} />
+      </Suspense>
     </div>
   )
 }
